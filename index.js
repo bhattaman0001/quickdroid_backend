563b5034babcf978c6d6ff38f598f834ae963c6b
Aman Bhatt <Bhattaman0001@gmail.com>
2024-02-08 17:17:37 +0530

    First commit

--- index.js
+++ index.js
@@ -6,25 +6,22 @@ const router = require("./routes/routeJS");
 const app = express();
 const mongoose = require("mongoose");
 
-// connection with DB
-// connectMongoDB("mongodb+srv://pqrst:abc@cluster0.r6wyviz.mongodb.net/aman")
 connectMongoDB(
-  "mongodb+srv://amanbhatt02:aman2001@cluster0.ey10qw7.mongodb.net/bck"
+  "mongodb+srv://amanbhatt02:aman2001@cluster0.ey10qw7.mongodb.net/quickdroid_backend"
 )
   .then(async () => {
     console.log("Database connected!!");
-    // Access the collection directly and fetch all documents
-    const collection = mongoose.connection.db.collection("bck"); // Replace 'your_collection_name' with your actual collection name
+    /* // Access the collection directly and fetch all documents
+    const collection = mongoose.connection.db.collection("files"); // Replace 'your_collection_name' with your actual collection name
     const allData = await collection.find({}).toArray();
-
     // Print all JSON data
-    console.log("All data:", allData);
+    console.log("All data:", allData); */
   })
   .catch((error) => {
     return console.log(`Error in DB connection is: ${error}`);
   });
 
-// middleware plugin
+// middleware plugin 
 app.use(express.urlencoded({ extended: false }));
 app.use(logReqRes("log.txt"));
 app.use("/files", router);
